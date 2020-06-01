873c9c4a681aeb46249ccc503ee08967891dd24c
EdersonSouza <ederson.souza.mg@gmail.com>
2020-06-01 16:21:50 -0400

    configurando cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-//import cors from 'cors';
+import cors from 'cors';
 
 import routes from "./routes";
 import databaseConfig from "./config/database";
@@ -8,11 +8,11 @@ import databaseConfig from "./config/database";
 class App {
   constructor() {
     this.express = express();
-
+    this.cors();
     this.database();
     this.middlewares();
     this.routes();
-    this.cors();
+    
   }
 
   database() {
@@ -25,14 +25,8 @@ class App {
   middlewares() {
     this.express.use(express.json());
   }
+  
   cors() {
-    this.express.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-      res.header("Acess-Control-Allow-Header", "Content-Type", "Authorization")
-      next();
-    });
-  }
-  /*cors() {
     console.log("estou aqui")
     this.express.use(cors({
       origin: ["http://localhost:8080", "http://192.168.1.5:8080"],
@@ -40,7 +34,7 @@ class App {
       allowedHeaders: ["Content-Type", "Authorization"],
       
     }));
-  }*/
+  }
 
   routes() {
     this.express.use(routes);
